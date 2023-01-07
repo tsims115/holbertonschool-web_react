59d93fd9c1fc0562ae9dec83d0460eab3be7f197
Trenton Sims <tsims115@yahoo.com>
2023-01-06 21:53:48 -0600

    Adding task 2

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        use: ["file-loader", "image-webpack-loader"],
       },
     ],
   },

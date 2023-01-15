e3cb729a013d20ac623ad593578011f18c0b9a53
Trenton Sims <tsims115@yahoo.com>
2023-01-14 19:40:54 -0600

    adding task 5

---
+++ 0x02-react_props/task_5/dashboard/src/CourseList/CourseList.test.js
@@ -0,0 +1,20 @@
+import CourseList from "./CourseList";
+import { shallow } from 'enzyme';
+import React from 'react';
+const expect = require('chai').expect;
+
+
+
+describe('<CourseListRow />', () => {
+  it("renders CourseListRow without crashing", () => {
+    shallow(<CourseList />);
+  });
+  it("renders 3 rows when given nothing", () => {
+    const SCourseList = shallow(<CourseList />);
+    expect(SCourseList.find('CourseListRow').length).to.equal(3);
+  });
+  it("renders 3 rows when given a course", () => {
+    const SCourseList = shallow(<CourseList listCourses={[{id: 2, name: 'React', credit:68}]} />);
+    expect(SCourseList.find('CourseListRow').length).to.equal(3);
+  });
+});
---
+++ 0x02-react_props/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,76 @@
+import { shallow } from 'enzyme';
+import Notifications from './Notifications.js';
+import React from 'react';
+const expect = require('chai').expect;
+
+const listNotifications = [
+  {
+  value: 'Someone wants to connect!',
+  id: 0,
+  type: 'default',
+  },
+  {
+  value: 'Employer wants to reach out',
+  id: 1,
+  type: 'urgent',
+  },
+  {
+  value: '',
+  id: 2,
+  type: 'urgent',
+  html: {__html: 'This is a string'}
+  },
+]
+
+describe('<Notifications />', () => {
+  it("renders without crashing", () => {
+    shallow(<Notifications />);
+  });
+  it("Notification renders 3 NotificationItems when given list", () => {
+    const notificationsComponent = shallow(<Notifications listNotifications={listNotifications} />);
+    expect(notificationsComponent.find('NotificationItem').length).to.equal(3);
+  });
+  it("Notification renders a correct header text", () => {
+    const SComponent = shallow(<Notifications />);
+    expect(SComponent.contains('Here is the list of notifications')).to.equal(true);
+  });
+  it("Notification renders menuItem div", () => {
+    const SComponent = shallow(<Notifications />);
+    expect(SComponent.find('.menuItem').length).to.equal(1);
+  });
+  it("Does not display div.Notifications when display drawer is false", () => {
+    const SComponent = shallow(<Notifications />);
+    expect(SComponent.find('.Notifications').props()['style']).to.eql({display: "none"});
+  });
+  it("Displays div.Notifications when display drawer is true", () => {
+    const SComponent = shallow(<Notifications displayDrawer={true} />);
+    expect(SComponent.find('.Notifications').props()['style']).to.eql({display: "block"});
+  });
+
+});
+
+describe('<Notifications /> when given a list of notifications', () => {
+  const listNotifications = [
+    {
+    value: 'Someone wants to connect!',
+    id: 0,
+    type: 'default',
+    },
+    {
+    value: 'Employer wants to reach out',
+    id: 1,
+    type: 'urgent',
+    },
+    {
+    value: '',
+    id: 2,
+    type: 'urgent',
+    html: {__html: 'This is a string'}
+  },
+]
+const SComponent = shallow(<Notifications displayDrawer={true} listNotifications={listNotifications} />);
+
+  it("Displays div.Notifications when display drawer is true", () => {
+    expect(SComponent.find('.Notifications').props()['style']).to.eql({display: "block"});
+  });
+});

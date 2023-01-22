f90aebaf4b5bbc966336f86cfcb1439c50136765
Trenton Sims <tsims115@yahoo.com>
2023-01-21 19:02:24 -0600

    Adding changes

--- 0x00-react_state/task_0/dashboard/src/Notifications/Notifications.js
+++ 0x00-react_state/task_0/dashboard/src/Notifications/Notifications.js
@@ -92,6 +92,7 @@ class Notifications extends React.Component {
   }
 
   render()  {
+    
     if (this.props.displayDrawer) {
       this.display = (
         <div className={css(NotificationsStyle.notifications)} >
@@ -102,7 +103,7 @@ class Notifications extends React.Component {
           <div style={{textAlign: 'right'}}>
             <button 
               aria-label='Close' 
-              onClick={ this.props.handleHideDrawer() }
+              onClick={ this.props.handleHideDrawer }
               style={{marginLeft: 'auto', marginBottom: ''}}>
               <img src={close_icon} alt='close' style={{
                 height: '8px',

e4adcd8a550bd54fc4890e1b27358cca61241dec
Trenton Sims <tsims115@yahoo.com>
2023-01-23 21:32:35 -0600

    Adding task 8

--- 0x01-React_Immutable/8-seq.js
+++ 0x01-React_Immutable/8-seq.js
@@ -1,20 +1,20 @@
 import { Seq } from 'immutable';
 
 function printBestStudents(obj) {
-  const sequence = Seq(obj);
-  let student = sequence.filter((value) => value.score > 70);
+  const student = Seq(obj).filter((v) => v.score > 70).toJS();
   let firstName;
   let lastName;
 
-  student = student.toJS();
   Object.keys(student).map((key) => {
     firstName = student[key].firstName;
-    firstName = firstName.charAt(0).toUpperCase() + firstName.splice(1);
-    lastName = lastName.charAt(0).toUpperCase() + lastName.splice(1);
+    lastName = student[key].lastName;
+    firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+    lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
     student[key].firstName = firstName;
     student[key].lastName = lastName;
-    return student[key];
+    return 1;
   });
+  console.log(student);
 }
 
 export default printBestStudents;

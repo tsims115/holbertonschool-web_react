18febc7692678000121a291fbbedd682e418fad8
Trenton Sims <tsims115@yahoo.com>
2023-01-23 19:33:12 -0600

    Adding task 2

--- 0x01-React_Immutable/2-nested.js
+++ 0x01-React_Immutable/2-nested.js
@@ -1,5 +1,5 @@
 import { Map } from 'immutable';
 export default function accessImmutableObject(object, array) {
   const mapItem = Map(object);
-  return mapItem.getIn(array);
+  return mapItem.getIn(array, undefined);
 }
